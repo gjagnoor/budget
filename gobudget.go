2779844fcbe6f2f1b5dd36dd08f5da47ebb64176
jgrewal <jagnoorg@jagnoorgs-MacBook-Pro.local>
2021-12-10 18:55:19 -0500

    interfaces

--- gobudget.go
+++ gobudget.go
@@ -5,6 +5,7 @@ import "github.com/google/uuid"
 type User struct {
 	ID uuid.UUID `db:"id"`
 	Email string `db:"email"`
+	Joined string `db:"joined"`
 }
 
 type Income struct {
@@ -13,8 +14,8 @@ type Income struct {
 	Amount float32 `db:"amount"`
 	Category string `db:"category"`
 	Notes string `db:"notes"`
-	Date string `db:"date"`
-	UserID string `db:"userid"`
+	Created string `db:"created"`
+	UserID string `db:"user_id"`
 }
 
 type Expense struct {
@@ -23,6 +24,27 @@ type Expense struct {
 	Amount float32 `db:"amount"`
 	Category string `db:"category"`
 	Notes string `db:"notes"`
-	Date string `db:"date"`
-	UserID string `db:"userid"`
+	Created string `db:"created"`
+	UserID string `db:"user_id"`
+}
+
+type UserStore interface {
+	User(id uuid.UUID) (User, error)
+	Users() ([]User, error)
+	CreateUser(u *User) error 
+	DeleteUser(id uuid.UUID) error
+}
+
+type IncomeStore interface {
+	Income(id uuid.UUID) (Income, error)
+	IncomesByUser(userID uuid.UUID) ([]Income, error)
+	CreateIncome(i *Income) error 
+	DeleteIncome(id uuid.UUID) error
+}
+
+type ExpenseStore interface {
+	Expense(id uuid.UUID) (Expense, error)
+	ExpensesByUser(userID uuid.UUID) ([]Expense, error)
+	CreateExpense(i *Expense) error 
+	DeleteExpense(id uuid.UUID) error
 }
